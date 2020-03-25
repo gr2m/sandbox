2673e579c15a6608913605adc8de7ca8efd410b5
Gregor Martynus <gregor@martynus.net>
2020-03-25 15:51:40 -0700

    debug: request workflow file

--- debug.js
+++ debug.js
@@ -6,7 +6,16 @@ async function run() {
   const octokit = new Octokit();
 
   try {
-    const { data } = octokit.request("/");
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+    workflow_file_name = "debug.yml";
+    const { data } = await octokit.request(
+      "GET /repos/:owner/:repo/actions/workflows/:workflow_file_name",
+      {
+        owner,
+        repo,
+        workflow_file_name
+      }
+    );
     console.log(data);
   } catch (error) {
     console.error(error);
